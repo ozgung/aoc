c7fc9ece0481c9f754807f365fdc3895ff8e3796
Ozgun Genc <ozgungenc@gmail.com>
2023-12-08 18:35:07 +0300

    add template

---
+++ template.py
@@ -0,0 +1,30 @@
+class Solution:
+    def __init__(self, filename):
+        self.answer = None
+        self.answers = []
+        self.parse_input(filename)
+
+    def parse_input(self, filename):
+        pass
+
+    
+    def solve(self):
+        pass
+
+
+if __name__ == '__main__':
+    # test
+    filename = "input/daytest.txt"
+    solution = Solution(filename)
+    solution.solve()
+    print("Test: ", solution.answers)
+    print("Test: ", solution.answer)
+    assert solution.answer == 0
+    
+    # real solution
+    filename = "input/day.txt"
+    solution = Solution(filename)
+    solution.solve()
+    print("Solution: ", solution.answers)
+    print("Solution: ", solution.debug)
+    print("Solution: ", solution.answer)
